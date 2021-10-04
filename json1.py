bf9041f0fdafb3f452e5d25433e049359d8882f0
Annabelle Herschend <annabelle_herschend1@baylor.edu>
2021-10-04 17:16:02 -0500

    json

--- json1.py
+++ json1.py
@@ -1,6 +1,8 @@
 import json
+from plotly.graph_objs import Scattergeo, Layout
+from plotly import offline
 
-infile = open('eq_data_1_day_m1.json', 'r')
+infile = open('eq_data_30_day_m1.json', 'r')
 outfile = open('readable_eq_data.json','w')
 
 eqdata = json.load(infile) 
@@ -23,13 +25,28 @@ lons = []
 for eq in list_of_eqs:
     mag = eq['properties']['mag']
     mags.append(mag)
-    lat = eq['geometry']['coordinates'][0]
-    lon = eq['geometry']['coordinates'][1]
+    lat = eq['geometry']['coordinates'][1]
+    lon = eq['geometry']['coordinates'][0]
     lats.append(lat)
     lons.append(lon)
 
-print(mags)
-print(lats)
-print(lons)
+
+# to create a list [0:5] saying you want the first 5 elements
+print(mags[:5])
+print(lats[:5])
+print(lons[:5])
 
 
+from plotly.graph_objs import Scattergeo, layout
+from plotly import offline
+
+
+data = [Scattergeo(lon=lons,lat=lats)]
+
+my_layout = Layout(title="Global Earthquakes 30 Day")
+
+fig = {'data':data,'layout':my_layout}
+
+
+#This will great a file in our project so we can view it offline
+offline.plot(fig,filename='globalearthquakes30day.html')
